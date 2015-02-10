1baafe0cad94ff6b57709a5d142b9e4974e92c6a
phpusr <phpusr@gmail.com>
2015-02-10 19:18:59 +0500

    change: Reset migration

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,16 +1,18 @@
 databaseChangeLog = {
 
-	changeSet(author: "phpusr (generated)", id: "1423473578400-1") {
-		createTable(tableName: "post") {
-			column(autoIncrement: "true", name: "id", type: "bigint") {
-				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "postPK")
-			}
+    changeSet(author: "phpusr (generated)", id: "1423577817347-1") {
+        createTable(tableName: "post") {
+            column(autoIncrement: "true", name: "id", type: "bigint") {
+                constraints(nullable: "false", primaryKey: "true", primaryKeyName: "postPK")
+            }
 
-			column(name: "version", type: "bigint") {
-				constraints(nullable: "false")
-			}
-		}
-	}
+            column(name: "version", type: "bigint") {
+                constraints(nullable: "false")
+            }
 
-	include file: 'v0.1.groovy'
+            column(name: "body", type: "longvarchar")
+
+            column(name: "title", type: "varchar(255)")
+        }
+    }
 }
