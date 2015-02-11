a76a07b61776665bee7a7960d04e94d57116958e
phpusr <phpusr@gmail.com>
2015-02-11 11:50:47 +0500

    add: v0.1 changelog

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,18 +1,4 @@
 databaseChangeLog = {
 
-    changeSet(author: "phpusr (generated)", id: "1423577817347-1") {
-        createTable(tableName: "post") {
-            column(autoIncrement: "true", name: "id", type: "bigint") {
-                constraints(nullable: "false", primaryKey: "true", primaryKeyName: "postPK")
-            }
-
-            column(name: "version", type: "bigint") {
-                constraints(nullable: "false")
-            }
-
-            column(name: "body", type: "longvarchar")
-
-            column(name: "title", type: "varchar(255)")
-        }
-    }
+	include file: 'v0.1.groovy'
 }
---
+++ grails-app/migrations/v0.1.groovy
@@ -0,0 +1,18 @@
+databaseChangeLog = {
+
+    changeSet(author: "phpusr (generated)", id: "1423637346885-1") {
+        createTable(tableName: "post") {
+            column(autoIncrement: "true", name: "id", type: "bigint") {
+                constraints(nullable: "false", primaryKey: "true", primaryKeyName: "postPK")
+            }
+
+            column(name: "version", type: "bigint") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "body", type: "longtext")
+
+            column(name: "title", type: "varchar(255)")
+        }
+    }
+}
