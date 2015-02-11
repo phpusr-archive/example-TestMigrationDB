e11772876453b50df73c6efdad5592bc29474a9c
phpusr <phpusr@gmail.com>
2015-02-11 12:44:02 +0500

    update: Changes

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,4 +1,8 @@
 databaseChangeLog = {
 
+    changeSet(author: "phpusr (generated)", id: "changelog") {
+        // TODO add changes and preconditions here
+    }
+
 	include file: 'v0.1.groovy'
 }
--- grails-app/migrations/v0.1.groovy
+++ grails-app/migrations/v0.1.groovy
@@ -1,18 +1,18 @@
 databaseChangeLog = {
 
-    changeSet(author: "phpusr (generated)", id: "1423637346885-1") {
-        createTable(tableName: "post") {
-            column(autoIncrement: "true", name: "id", type: "bigint") {
-                constraints(nullable: "false", primaryKey: "true", primaryKeyName: "postPK")
-            }
+	changeSet(author: "phpusr (generated)", id: "1423639868355-1") {
+		createTable(tableName: "post") {
+			column(autoIncrement: "true", name: "id", type: "bigint") {
+				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "postPK")
+			}
 
-            column(name: "version", type: "bigint") {
-                constraints(nullable: "false")
-            }
+			column(name: "version", type: "bigint") {
+				constraints(nullable: "false")
+			}
 
-            column(name: "body", type: "longtext")
+			column(name: "body", type: "longtext")
 
-            column(name: "title", type: "varchar(255)")
-        }
-    }
+			column(name: "title", type: "varchar(255)")
+		}
+	}
 }
